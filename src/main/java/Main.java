f8baa37ed526a3eb403022238e17c04d1a5ed2b0
Josue07-maker <josuevegacajas@gmail.com>
2026-04-22 17:34:11 +0200

    Añadido un README

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main (String[] args){
-        System.out.println("Hola 2º DAM!");
+        System.out.println("Bienvenida al control de versiones de git");
         System.out.println("Adios 2º DAM!");
     }
 }
