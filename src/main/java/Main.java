bde6776cc14a83b9eb7660c1fa179c0374c9072c
Alberto <izancascon02@gmail.com>
2026-04-24 19:52:13 +0200

    cambios de numero random

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,8 @@ public class Main{
     public static void main (String[] args){
         System.out.println("Bienvenida al control de versiones de git");
         System.out.println("Adios 2º DAM!");
+
+        
+        System.out.println("mnumero random: " + Math.random());
     }
 }
