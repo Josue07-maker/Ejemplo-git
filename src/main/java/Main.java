edbd0f5e84edfe47a04bc766e8e1a8e5ecd2e203
Josue07-maker <josuevegacajas@gmail.com>
2026-04-24 19:44:46 +0200

    Cambio saludo feature/cambio-saludo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,7 @@ public class Main{
     public static void main (String[] args){
         System.out.println("Bienvenida al control de versiones de git");
         System.out.println("Adios 2º DAM!");
+        System.out.println("Hola desde la rama feature/cambio-saludo");
+
     }
 }
