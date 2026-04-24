9bcf8ca57cac88112827fedec00fec9d98b4e554
Alberto <izancascon02@gmail.com>
2026-04-24 19:58:19 +0200

    waos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ public class Main{
         System.out.println("Bienvenida al control de versiones de git");
         System.out.println("Adios 2º DAM!");
 
-        
         System.out.println("mnumero random: " + Math.random());
     }
 }
